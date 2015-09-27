2ffcf6d48a00878cc956a9f3b59c36529e110caa
Leohongyi <wenhualiu.liu@gmail.com>
2015-09-26 23:41:52 -0400

    Initial Commit

---
+++ Personal Contact/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Personal Contact
+//
+//  Created by apple on 9/26/15.
+//  Copyright © 2015 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Personal Contact/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Personal Contact
+//
+//  Created by apple on 9/26/15.
+//  Copyright © 2015 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
